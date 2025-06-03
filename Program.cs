0ec3787c5af96a4b40f81fbf5b5679762f0fddd8
gabs1san <sanbiel398@gmail.com>
2025-06-03 18:46:51 -0300

    adiciona exemplo de uso de HashSet e SortedSet com operações de conjunto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Conjuntos
 {
@@ -6,7 +7,44 @@ namespace Conjuntos
     {
         public static void Main(string[] args) 
         { 
-            
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            PrintCollction(a);
+
+            SortedSet<int> c = new SortedSet<int>(a); //union
+            c.UnionWith(b);
+            PrintCollction(c);
+
+            SortedSet<int> d = new SortedSet<int>(a); //intersection
+            d.IntersectWith(b);
+            PrintCollction(d);
+
+            SortedSet<int> e = new SortedSet<int>(a); //difference
+            e.ExceptWith(b);
+            PrintCollction(e);
+        }
+
+        static void PrintCollction<T>(IEnumerable<T> collection)
+        {
+            foreach ( T obj in collection)
+            {
+                Console.Write(obj + "");
+            }
+            Console.WriteLine();
         }
     }
 }
